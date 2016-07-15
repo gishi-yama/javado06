3426057b7141ff34310cbf8db466f104d1a60606
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2016-07-15 22:39:38 +0900

    Change to PathParameter

--- src/main/java/javado/lec06/CalcResource.java
+++ src/main/java/javado/lec06/CalcResource.java
@@ -1,25 +1,22 @@
 package javado.lec06;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 @Path("calc")
 public class CalcResource {
 
-    @Path("add")
+    @Path("add/{a}/{b}")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public int add(@QueryParam("a") int a, @QueryParam("b") int b) {
+    public int add(@PathParam("a") int a, @PathParam("b") int b) {
         return a + b;
     }
 
-    @Path("div")
+    @Path("div/{a}/{b}")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public int div(@QueryParam("a") int a, @QueryParam("b") int b) {
+    public int div(@PathParam("a") int a, @PathParam("b") int b) {
         return a * b;
     }
 }
--- src/test/java/javado/lec06/HogeTest.java
+++
@@ -1,7 +0,0 @@
-package javado.lec06;
-
-import org.glassfish.jersey.test.JerseyTest;
-
-public class HogeTest extends JerseyTest {
-
-}
